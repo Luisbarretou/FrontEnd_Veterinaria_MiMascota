fb90e9b9cd35b80f5d333d626a2934c6545ceac1
LuisBarretoU20 <luisbarretourquiza.20@gmail.com>
2023-06-05 09:50:31 -0500

    Adicion de Modulo Servicio

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -20,16 +20,16 @@ export class ClienteService {
         return this.httpClient.get<Cliente[]>(`${this.urlBackend}/habilitados`)
     }
 
-    obtenerCliente(id: number): Observable<Cliente> {
-        return this.httpClient.get<Cliente>(`${this.urlBackend}/${id}`);
-    }
-
     crearCliente(cliente: Cliente): Observable<Object> {
         return this.httpClient.post(`${this.urlBackend}`, cliente)
     }
 
     actualizarCliente(id: number, cliente: Cliente): Observable<object> {
         return this.httpClient.put(`${this.urlBackend}/${id}`, cliente);
+    }
+
+    obtenerCliente(id: number): Observable<Cliente> {
+        return this.httpClient.get<Cliente>(`${this.urlBackend}/${id}`);
     }
 
     eliminarCliente(id: number): Observable<Object> {
--- src/app/especialistas/app/historias/historia.ts
+++
@@ -1,8 +0,0 @@
-import { Mascota } from "../mascotas/mascota";
-
-export class Historia {
-    historiaId: number;
-    historiaFechaCreacion: string;
-    historiaEstado: string;
-    mascota: Mascota;
-}
