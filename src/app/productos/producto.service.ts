a543caa6f793688308c208aff92f783da5c96506
LuisBarretoU <luis_barreto_20@hotmail.com>
2023-06-06 19:18:59 -0500

    adicion de metodos para service de producto

--- src/app/productos/producto.service.ts
+++ src/app/productos/producto.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Producto } from './producto';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ProductoService {
 
-  constructor() { }
+    private urlBackend = "http://localhost:7071/api/productos";
+
+    constructor(private httpClient: HttpClient) { }
+
+    obtenerListaProductos(): Observable<Producto[]> {
+        return this.httpClient.get<Producto[]>(`${this.urlBackend}`);
+    }
+
+    
 }
