26c14d5ef3ef6920e96c2d2c6a9eb0e2df17c9d5
LuisBarretoU <luis_barreto_20@hotmail.com>
2023-06-08 00:42:05 -0500

    Adicion de modulo Proveedores

--- src/app/proveedores/actualizar-proveedores/actualizar-proveedores.component.ts
+++ src/app/proveedores/actualizar-proveedores/actualizar-proveedores.component.ts
@@ -7,88 +7,85 @@ import { ActivatedRoute, Router } from '@angular/router';
 import Swal from 'sweetalert2';
 
 @Component({
-  selector: 'app-actualizar-proveedores',
-  templateUrl: './actualizar-proveedores.component.html',
-  styles: [
-  ]
+    selector: 'app-actualizar-proveedores',
+    templateUrl: './actualizar-proveedores.component.html',
+    styles: []
 })
 export class ActualizarProveedoresComponent {
 
+    proveedor: Proveedor = new Proveedor();
+    id: number;
 
-  proveedor: Proveedor = new Proveedor();
-  id: number;
+    estado: String[];
 
-  estado: String[];
+    items: MenuItem[];
+    home: MenuItem;
 
-  items: MenuItem[];
-  home: MenuItem;
+    constructor(private proveedorService: ProveedorService, private router: Router,
+        private activatedRoute: ActivatedRoute) { }
 
+    ngOnInit(): void {
+        this.id = this.activatedRoute.snapshot.params['id'];
+        this.proveedorService.obtenerProveedor(this.id).subscribe(dato => {
+            this.proveedor = dato;
+            Swal.fire(
+                'Actualización de Proveedor',
+                `Razon Social: ${this.proveedor.proveedorRazonSocial}`,
+                'info',
+            )
+        })
 
-  constructor(private proveedorService: ProveedorService, private router: Router,
-              private activatedRoute: ActivatedRoute){ }
+        this.estado = [
+            "Habilitado",
+            "Inhabilitado"
+        ];
 
+        this.items = [{ label: 'Proveedor', routerLink: '/proveedores' }, { label: 'Actualización ' }];
+        this.home = { icon: 'pi pi-home', routerLink: '/dashboard' };
+    }
 
-  ngOnInit(): void{
-    this.id= this.activatedRoute.snapshot.params['id'];
-    this.proveedorService.obtenerProveedores(this.id).subscribe(dato => {
-      this.proveedor = dato;
-      Swal.fire(
-        'Actulización de Proveedor',
-        `ID: ${this.proveedor.proveedorId}`,
-        'info',
-      )
-    })
-
-    this.estado =[
-      "Habilitado",
-      "Inhabilitado"
-    ];
-
-    this.items = [{label:'Proveedor', routerLink: '/proveedores'}, {label: 'Actualización '}];
-    this.home = {icon: 'pi pi-home', routerLink: '/dashboard'};
-  }
-
-  actualizarProveedores(){
-    Swal.fire({
-      title: 'Está seguro de los cambios realizados?',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Si, actualizar!'
-    }).then((result)=>{
-      if(result.isConfirmed){
-        Swal.fire(
-          'Proveedor Actualizado',
-          `ID: ${this.proveedor.proveedorId} ha sido actualizado con exito`,
-          'success'
-        )
-          this.proveedorService.actualizarProveedores(this.id, this.proveedor).subscribe(dato=>{
-          this.router.navigate(['./proveedores']);
-          })
-      }
-    })
-  }
+    actualizaProveedor() {
+        Swal.fire({
+            title: 'Está seguro de los cambios realizados?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, actualizar!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Proveedor Actualizado',
+                    `El proveedor: ${this.proveedor.proveedorId} ha sido actualizado con éxito`,
+                    'success'
+                )
+                this.proveedorService.actualizarProveedor(this.id, this.proveedor).subscribe(dato => {
+                    this.router.navigate(['./proveedores']);
+                })
+            }
+        })
+    }
 
 
-  regresarListaProveedores(){
-    Swal.fire({
-      title: 'Está seguro de cancelar la operación?',
-      text: "Regresarás a la lista de Proveedores!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Si, quiero cancelar la actualización!'
-  }).then((result) => {
-      if (result.isConfirmed) {
-      Swal.fire(
-          'Operación cancelada!',
-          'Has sido redirigido a la lista de Proveedores.',
-          'error'
-      )
-      this.router.navigate(['./proveedores']);}
-  })
+    regresaListaProveedores() {
+        Swal.fire({
+            title: 'Está seguro de cancelar la operación?',
+            text: "Regresarás a la lista de Proveedores!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, quiero cancelar la actualización!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Operación cancelada!',
+                    'Has sido redirigido a la lista de Proveedores.',
+                    'error'
+                )
+                this.router.navigate(['./proveedores']);
+            }
+        })
 
-  }
+    }
 }
