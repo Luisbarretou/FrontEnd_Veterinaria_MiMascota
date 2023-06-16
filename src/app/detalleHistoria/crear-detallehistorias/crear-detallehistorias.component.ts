aaafe033f2c678e6a046750fb2bbb7f8cd51c0f3
LuisBarretoU <luis_barreto_20@hotmail.com>
2023-06-16 09:22:55 -0500

    creacion de componentes detalleHistoria

---
+++ src/app/detalleHistoria/crear-detallehistorias/crear-detallehistorias.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-detallehistorias',
+  templateUrl: './crear-detallehistorias.component.html',
+  styles: [
+  ]
+})
+export class CrearDetallehistoriasComponent {
+
+}
